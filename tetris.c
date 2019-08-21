6ba26c58586e9aa532d1a0faddace9008bd5c654
jadiloiro <jadielga13@gmail.com>
2019-08-21 12:23:49 -0300

    commit 21-08-19

--- tetris.c
+++ tetris.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define COLUMS 60
+#define ROWS 25
+
+void init(char matrix[ROWS][COLUMS]){
+    int i, j ;
+
+    for(i=0; i<ROWS; i++){
+        for(j=0; j<COLUMS; j++){
+            matrix[i][j] = ' ';
+        }
+    }
+}
+
+
+void printMatrix(char matrix[ROWS][COLUMS]){
+        int i, j ;
+
+    for(i=0; i<ROWS; i++){
+        for(j=0; j<COLUMS; j++){
+        printf("%c", matrix[i][j]);
+        }
+
+        printf("\n");
+    }
+
+}
 
 int main () {
 
-    printf("HELO MUNDO");
+    char matrix[ROWS][COLUMS];
+    int posI, posJ;
+
+    posI = ROWS/2;
+    posJ = COLUMS/2;
+
+    system("cls");
+
+    init(matrix);
+
+    while(1){
+        system("cls");
+        matrix[posI][posJ] = '@';
+
+        printMatrix(matrix);
+        matrix[posI][posJ] = ' ';
+        if(posJ < COLUMS) posJ++;
+
+    }
+
+    system("pause");
+
     return 0;
 }
