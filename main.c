a1e5fd4614a9f74d3eca238a0a109f55f1db756f
jadiloiro <jadielga13@gmail.com>
2019-09-09 11:58:41 -0300

    commit atrasado

--- main.c
+++ main.c
@@ -1,26 +1,46 @@
-//Projeto do jogo Tetris implementado em "C" para uso no terminal do console
-//Para executar 
-//No cmd : start programa.exe
-//Autor : Jadiel G. Farias Jr.
-//Data : 28/08/2019
-
+/*
+    Jogo interativo Tetris implementado em linguagem C para uso 
+    no console (terminal de comandos)
+    Para executar:
+    > start programa.exe
+    Autor: Jadiel Farias junior
+    Data: 28/08/2019
+*/
 
 #include "tetris.h"
-//Parte do programa responsavel por iniciar as funções
-int main () {
+#include "display.h"
+
+/*
+    Parte principal do programa, responsável por iniciar e 
+    chamar as funções auxiliares.
+*/
+int main(){
     char matrix[ROWS][COLUMS];
     int posI, posJ;
+
+    //posicao inicial do personagem
     posI = ROWS/2;
     posJ = COLUMS/2;
-    system("cls");
+    //inicializando matriz
     init(matrix);
-    while(1){
-        system("cls");
+
+    //apagar o cursor da tela
+    ShowConsoleCursor(0);
+    system("cls");
+
+    while(1){        
+        gotoxy(0,0);
+
         matrix[posI][posJ] = '@';
+
         printMatrix(matrix);
+
         matrix[posI][posJ] = ' ';
+        
         if(posJ < COLUMS) posJ++;
     }
+
     system("pause");
+
     return 0;
 }
